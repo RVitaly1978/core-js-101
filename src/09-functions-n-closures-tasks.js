0ca43ac5e95fa317ae5dcbe5c9fbf9e813293969
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-04-28 21:36:39 +0300

    feat: solve all task in functions

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -189,8 +189,16 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const args = [...args1];
+
+  return function inner(...args2) {
+    if (args2.length) {
+      args.push(...args2);
+    }
+
+    return fn.call(null, ...args);
+  };
 }
 
 
@@ -211,8 +219,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom - 1;
+
+  return function inner() {
+    id += 1;
+
+    return id;
+  };
 }
 
 
